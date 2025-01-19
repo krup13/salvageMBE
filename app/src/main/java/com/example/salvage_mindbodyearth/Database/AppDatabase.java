d3a45c2a3767fb90fd57745d37f62a5a77031229
krup13 <khairanafisa4@gmail.com>
2025-01-20 03:02:03 +0800

    lalala commit

--- app/src/main/java/com/example/salvage_mindbodyearth/Database/AppDatabase.java
+++ app/src/main/java/com/example/salvage_mindbodyearth/Database/AppDatabase.java
@@ -3,9 +3,10 @@ package com.example.salvage_mindbodyearth.Database;
 import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 @Database(entities = {}, version = 1)
-public abstract class AppDatabase {
+public abstract class AppDatabase extends RoomDatabase {
 
     private static volatile AppDatabase INSTANCE;
 
