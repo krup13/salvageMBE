47b0993f2d072a29b100fb4cb9e4d2d9af8363df
krup13 <khairanafisa4@gmail.com>
2025-01-21 18:26:27 +0800

    homepage working

--- app/src/main/java/com/example/salvage_mindbodyearth/Activities/MainActivity.java
+++ app/src/main/java/com/example/salvage_mindbodyearth/Activities/MainActivity.java
@@ -1,7 +1,29 @@
 package com.example.salvage_mindbodyearth.Activities;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
+import com.example.salvage_mindbodyearth.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, WorkoutPlanningActivity.class);
+
+        findViewById(R.id.bubble_workout_plan).setOnClickListener(v -> {
+            startActivity(intent);
+        });
+
+    }
 }
